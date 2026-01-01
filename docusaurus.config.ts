b9327fcf23e0337998da8c8d7066649d15417308
Melvin Carvalho <melvincarvalho@gmail.com>
2026-01-01 19:14:44 +0100

    Default to light theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
     colorMode: {
       defaultMode: 'light',
       disableSwitch: false,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
 
     announcementBar: {
